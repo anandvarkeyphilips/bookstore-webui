860f0f61a2da094ecdd710b27c98f15632789d63
Anand Varkey Philips <anandvarkeyphilips@gmail.com>
2024-07-25 13:07:55 +0530

    Search with title or author

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,60 +1,63 @@
-import { useEffect, useState } from "react";
-//import books from '../data/books';
-import BookCardView from "./partials/BookCardView";
-import BookTableView from "./partials/BookTableView";
-import axios from "axios";
-
+import { useEffect, useState } from 'react';
+import BookCardView from './partials/BookCardView';
+import BookTableView from './partials/BookTableView';
+import axios from 'axios';
+ 
 export default function Home() {
-  // console.log(books);
-
-  const [viewType, setViewType] = useState("table");
-  const [books, setBooks] = useState([]);
-
-  const viewSwitchHandler = (e) => {
-    if (e.target.checked) {
-      setViewType("card");
-    } else {
-      setViewType("table");
+    const [viewType, setViewType] = useState('table');
+    const [books, setBooks] = useState([]); // full list of books
+    const [filteredBooks, setFilteredBooks] = useState([]); // filtered list of books
+    const [searchText, setSearchText] = useState('');
+ 
+    const viewSwitchHandler = (e)=> {
+        if(e.target.checked) {
+            setViewType('card');
+        } else {
+            setViewType('table');
+        }
     }
-  };
-
-  useEffect(() => {
-    // fetch("http://localhost:5000/books")
-    //   .then((res) => res.json())
-    //   .then((data) => setBooks(data))
-    //   .catch((reason) => console.error(reason));
-
-    axios.get("http://localhost:5000/books")
-      .then((res) => setBooks(res.data))
-      .catch((reason) => console.error(reason));
-  },[]);
-
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-12">
-          <div class="form-check form-switch">
-            <input
-              class="form-check-input"
-              type="checkbox"
-              onChange={viewSwitchHandler}
-              role="switch"
-              id="viewType"
-            />
-            <label class="form-check-label">Card View</label>
-          </div>
+ 
+    const searchHandler = (e)=> {
+        setSearchText(e.target.value);
+        let temp = books.filter(book => (book.title.toLowerCase().includes(e.target.value.toLowerCase()) 
+        || book.author.toLowerCase().includes(e.target.value.toLowerCase())));
+        setFilteredBooks(temp);
+    }  
+   
+    useEffect(()=>{
+        // fetch('http://localhost:5000/books')
+        //     .then(res => res.json())
+        //     .then(data => setBooks(data))
+        //     .catch(error => console.error(error));
+ 
+        axios.get('http://localhost:5000/books')
+            .then(res => setBooks(res.data))
+            .catch(error => console.error(error));
+    },[])
+ 
+    useEffect(()=>{
+        setFilteredBooks(books);
+    },[books])
+ 
+    return (
+        <div className='container'>
+            <div className='row alert alert-info mt-2'>
+                <div className='col-sm-12 col-md-6 col-lg-3'>
+                    <div className="form-check form-switch">                        
+                        <input className="form-check-input" type="checkbox" onChange={viewSwitchHandler} role="switch" id="viewType" />
+                        <label className="form-check-label" >Card View</label>
+                    </div>
+                </div>
+                <div className='col-sm-12 col-md-6 col-lg-6'>
+                    <input type='text' value={searchText} onChange={searchHandler} className='form-control' placeholder='Search Books or Author' />
+                </div>
+            </div>
+            <div className='row'>
+                <div className='col-12'>
+                    <h2>List of Books</h2>
+                    { viewType === 'table' ? <BookTableView books={filteredBooks} /> : <BookCardView books={filteredBooks} /> }
+                </div>
+            </div>
         </div>
-      </div>
-      <div className="row">
-        <div className="col-12">
-          <h2>List of Books</h2>
-          {viewType === "table" ? (
-            <BookTableView books={books} />
-          ) : (
-            <BookCardView books={books} />
-          )}
-        </div>
-      </div>
-    </div>
-  );
+    )
 }
