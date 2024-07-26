0fad5172bb08eab144808aaeb77849e5d159ea69
Anand Varkey Philips <anandvarkeyphilips@gmail.com>
2024-07-26 12:01:21 +0530

    Add toast

--- src/components/CreateBook.jsx
+++ src/components/CreateBook.jsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { Toast } from "bootstrap";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -36,7 +37,10 @@ export default function CreateBook() {
           year: 2024,
           language: "",
         });
-        navigate("/");
+        //navigate("/");
+        let toastEl = document.getElementById("liveToast");
+        let toast = new Toast(toastEl, { autohide: true, delay: 3000 });
+        toast.show();
       })
       .catch((error) => {
         alert(error);
@@ -122,6 +126,28 @@ export default function CreateBook() {
               Create
             </button>
           </form>
+        </div>
+      </div>
+      <div className="position-fixed bottom-0 end-0 p-3" style={{ zIndex: 11 }}>
+        <div
+          id="liveToast"
+          className="toast hide"
+          role="alert"
+          aria-live="assertive"
+          aria-atomic="true"
+        >
+          <div className="toast-header">
+            {/* <img src="..." className="rounded me-2" alt="..." /> */}
+            <strong className="me-auto">Bookstore</strong>
+            {/* <small>11 mins ago</small> */}
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="toast"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div className="toast-body">New book has been added successfully</div>
         </div>
       </div>
     </div>
